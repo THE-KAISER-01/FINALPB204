596585e9e136af5417ecba71ca8ebd40905dbabf
THE-KAISER-01 <jesusabdias25@gmail.com>
2024-08-13 14:38:18 -0500

    Edad y prueba de datos

---
+++ edad.py
@@ -0,0 +1,24 @@
+
+#edad = int(input("ingrese su edad"))
+
+#if edad < 18:
+#    print("no puedes pasar")
+#else:
+#    print("bienvenido")
+
+#varios tipos de constantes
+
+PI = 3.1415926535
+
+data = [3.1415926535]
+PI = frozenset(data)
+
+VALORES = {"pi":3.1415926535}
+print(VALORES["pi"])
+
+print("hola mundo")
+
+flotante = float(input())
+pinocho = str(input())
+entero = int(input())
+
