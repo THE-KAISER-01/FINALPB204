b71e30eb41f653d5c0a3831b65ffe0bf985bf2f5
THE-KAISER-01 <jesusabdias25@gmail.com>
2024-08-14 14:50:46 -0500

    if/elif/else

--- edad2.py
+++ edad2.py
@@ -1,6 +1,12 @@
 edad = int(input("ingrese la edad: "))
 
-if edad <18:
-    print("no pasas")
+if edad >18:
+    print("pasas")
+    
+elif edad <18 and edad>16:
+    print("podrias pasar")
+
 else:
-    print("si pasas")
+    print("¿que haces aqui?")
+    
+
