6d34cedd646c3872d7db9230a30f53706de450b8
THE-KAISER-01 <jesusabdias25@gmail.com>
2024-08-15 15:01:06 -0500

    TAREA 2

--- TAREA 2/ejercicio3.py
+++ TAREA 2/ejercicio3.py
@@ -1,3 +1,13 @@
+def long(cadena):
 
-    
+    Longitud=0
+    for _ in cadena:
+        Longitud+=1
+    return Longitud
+
+cadena=input("dame la cadena: \n")
+print("la longitud de la cadena es: ", long(cadena))
+
+
+#aun falta la lista    
 
