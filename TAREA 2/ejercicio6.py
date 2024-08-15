c8fbf4f65afe6ee077277ad22da8a8afda01108e
THE-KAISER-01 <jesusabdias25@gmail.com>
2024-08-15 14:41:14 -0500

    TAREA 2

--- TAREA 2/ejercicio6.py
+++ TAREA 2/ejercicio6.py
@@ -1,7 +1,7 @@
 def inversa(cadena):
     Cadena_invertida = ""
-    for char in cadena:
-        Cadena_invertida=char+Cadena_invertida
+    for x in cadena:
+        Cadena_invertida=x+Cadena_invertida
     return Cadena_invertida
         
 print(inversa("ESTOY PROBANDO"))
--- TAREA 2/ejercicio9.py
+++ TAREA 2/ejercicio9.py
@@ -1,1 +1,9 @@
-palabra= input("dame un numero entero")
+def generar_n_caracteres():
+
+    x=int(input("ingrese el numero: "))
+    resultado="*" * x
+
+    print("cantidad:", resultado)
+
+generar_n_caracteres()
+
