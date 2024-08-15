7cefa9e74ae3087e734c18f035150dca6d091d30
JESUS ABDIAS <jesusabdias25@gmail.com>
2024-08-14 21:15:11 -0600

    TAREA 2

--- TAREA 2/ejercicio 1.py
+++
@@ -1,7 +0,0 @@
-x= int(input("ingrese el primer numero:"))
-y= int(input("ingrese el segundo numero:"))
-
-if x < y: 
-    print("el segundo numero es mayor que el primero")
-else:
-    print("el primer numero es mayor que el segundo")
---
+++ TAREA 2/ejercicio1.py
@@ -0,0 +1,9 @@
+x = int(input("ingrese el primer numero: "))
+y = int(input("ingrese el segundo numero: "))
+
+if x < y: 
+    print("el segundo numero es mayor que el primero")
+elif x>y:
+    print("el primer numero es mayor que el segundo")
+else:
+    print("ambos numeros son iguales")
