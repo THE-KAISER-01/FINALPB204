c53d8bb448418422c1c26d0efabcc029bd3e7c09
jesus abdias <jesusabdias25@gmail.com>
2024-08-18 15:51:12 -0600

    clase

---
+++ clase/practicas.py
@@ -0,0 +1,18 @@
+'''coleccion = "jesus"
+
+for i in coleccion:
+    print(i, end="")
+
+
+precio = float(input("precio"))
+descuento = precio * 0.15
+precio_final=(precio-descuento)
+
+print(f"precio final a pagar es de {precio_final:.2f} ")
+'''
+
+a=(input("digite el primer numero: "))
+b=(input("digite el segundo numero: "))
+c=(input("digite el tercero numero: "))
+
+if 
