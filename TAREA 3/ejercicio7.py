2e923c7aeceb96ef6476291fbabc01498bc6deac
JESUS ABDIAS <jesusabdias25@gmail.com>
2024-08-18 21:45:40 -0600

    TAREA 3

--- TAREA 3/ejercicio7.py
+++ TAREA 3/ejercicio7.py
@@ -1,4 +1,23 @@
-Tupla = (19,20,21,25,32)
+edades = (19,12,32,25,32,38,26,14,28,33)
+contador = 0
 
-if Tupla > 20:
-    print(Tupla)
+for edad in edades:
+    if edad > 20:
+        contador += 1
+        
+print("La cantidad de personas con edades superiores a 20 es:", contador)
+
+
+# parte dos del codigo donde pide las edades:
+edades_usuario = []
+for i in range(10):
+    edad = int(input(f"Ingrese la edad de la persona {i + 1}: "))
+    edades_usuario.append(edad)
+
+contador_usuario = 0
+
+for edad in edades_usuario:
+    if edad > 20:
+        contador_usuario += 1
+
+print("La cantidad de personas con edades superiores a 20 es:", contador_usuario)
---
+++ TAREA 3/ejercicio8.py
@@ -0,0 +1,8 @@
+lista = ["jesus", "abdias", "alejandro", "kevin", "leonel"]
+
+contador = 0
+for nombre in lista:
+    if nombre.lower().startswith('a')
+        contador +=1
+
+print('la cantidad de nombres que contiene "a" son: \n', contador)
